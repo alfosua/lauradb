fbebc5cee0bfea6b3ef4b5270f2993bcf244fc86
Alfonso Suarez <68227851+alfosua@users.noreply.github.com>
2022-05-13 18:14:52 -0400

    chore: format current cpp files with clang-format

--- dbms/src/lauradb.cpp
+++ dbms/src/lauradb.cpp
@@ -8,32 +8,33 @@ string read(tcp::socket &socket);
 void send(tcp::socket &socket, const string &message);
 
 int main() {
-  boost::asio::io_service io_service;
-  tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 1234));
-  tcp::socket socket(io_service);
+    boost::asio::io_service io_service;
+    tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 1234));
+    tcp::socket socket(io_service);
 
-  acceptor.accept(socket);
+    acceptor.accept(socket);
 
-  fmt::print("Welcome to LauraDB!\n");
-  fmt::print("Listening...\n");
+    fmt::print("Welcome to LauraDB!\n");
+    fmt::print("Listening...\n");
 
-  string request = read(socket);
-  fmt::print("Request: {}\n", request);
+    string request = read(socket);
+    fmt::print("Request: {}\n", request);
 
-  fmt::print("Responding to client...\n");
-  send(socket, "Welcome to this LauraDB instance! But sorry, I cannnot handle "
-               "anything yet...");
-  fmt::print("Request Finished.\n");
+    fmt::print("Responding to client...\n");
+    send(socket,
+         "Welcome to this LauraDB instance! But sorry, I cannnot handle "
+         "anything yet...");
+    fmt::print("Request Finished.\n");
 }
 
 string read(tcp::socket &socket) {
-  boost::asio::streambuf buf;
-  boost::asio::read_until(socket, buf, "\n");
-  string data = boost::asio::buffer_cast<const char *>(buf.data());
-  return data;
+    boost::asio::streambuf buf;
+    boost::asio::read_until(socket, buf, "\n");
+    string data = boost::asio::buffer_cast<const char *>(buf.data());
+    return data;
 }
 
 void send(tcp::socket &socket, const string &message) {
-  const string final_message = message + "\n";
-  boost::asio::write(socket, boost::asio::buffer(final_message));
+    const string final_message = message + "\n";
+    boost::asio::write(socket, boost::asio::buffer(final_message));
 }
